364d8a9b905d7259d962ead0ba70309a9b977f0a
Sudharshan <sudarshanbetha2004@gmail.com>
2026-07-18 15:03:26 +0530

    Update API URLs

--- house-rent-app/client/src/config.js
+++ house-rent-app/client/src/config.js
@@ -1,2 +1,2 @@
 // Central configuration for the client application
-export const API_BASE_URL = 'http://localhost:5001/api';
+export const API_BASE_URL = 'https://house-rent-app-s6kb.onrender.com/api';
--- house-rent-app/client/src/services/propertyService.js
+++ house-rent-app/client/src/services/propertyService.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
+import { API_BASE_URL } from '../config';
 
-const API_URL = 'http://localhost:5001/api/properties/';
+const API_URL = `${API_BASE_URL}/properties/`;
 
 // Get all properties with filters
 const getProperties = async (params = {}) => {
