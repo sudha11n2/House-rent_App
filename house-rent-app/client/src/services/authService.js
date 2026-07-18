0d43083574964fc556e5be1f0864ff55f973f5d4
Sudharshan <sudarshanbetha2004@gmail.com>
2026-07-18 15:14:03 +0530

    Fix production auth API URL

--- house-rent-app/client/src/services/authService.js
+++ house-rent-app/client/src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5001/api/auth/';
+const API_URL = 'https://house-rent-app-s6kb.onrender.com/api/auth/';
 
 // Register user
 const register = async (userData) => {
